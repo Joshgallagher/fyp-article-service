76d1944e73015d2a68603cf934a4e951f92c95b4
Josh <joshgallagher24@gmail.com>
2020-02-15 22:02:49 +0000

    Configured the config module and removed boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(6262);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  await app.listen(configService.get<number>('PORT'));
 }
 
 bootstrap();
