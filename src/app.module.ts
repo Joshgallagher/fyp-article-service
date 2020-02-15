4760b2bc4a8b90742ae005a69bf4e52ecea67c5f
Josh <joshgallagher24@gmail.com>
2020-02-15 22:44:49 +0000

    Configured Typeorm with ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -7,6 +9,20 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: process.env.NODE_ENV === 'test'
         ? '.env.test'
         : '.env'
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<String>('HOST'),
+        port: configService.get<Number>('PORT'),
+        username: configService.get<String>('USERNAME'),
+        password: configService.get<String>('PASSWORD'),
+        database: configService.get<String>('DATABASE'),
+        // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      } as TypeOrmModuleOptions),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
